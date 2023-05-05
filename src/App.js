5fd3e150c0e0ab3ed155f649b43ffe61db2c3643
Ayelén Iacovelli <iacovelli.ayelen@gmail.com>
2023-05-05 15:56:49 -0300

    agrego la ul como componente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AddTask from "./components/Task/AddTask";
 import CardMsg from "./components/Task/CardMsg";
 import CardTask from "./components/Task/CardTask";
 import ToDoListContext from "./components/context/ToDoListContext";
 import ButtonAll from "./components/button/ButtonAll";
 import { Container } from "./components/Wrapper/GeneralWrapperStyles";
+import List from "./components/Task/List";
 
 
 
 function App() {
+
+
 
   const { data, handleAdd, handleDelete, handleDeleteAll } = useContext(ToDoListContext)
   console.log(data);
@@ -17,21 +20,9 @@ function App() {
     <>
       <Container>
         <AddTask handleAdd={handleAdd} />
-        <ul>
-          {data.length > 0 ? (
-            data.map((item) => (
-              <CardTask key={item.id} item={item} handleDelete={handleDelete} />
-            ))
-          ) : (
-            <CardMsg message="No hay tareas pendientes" />
-          )
-          }
-        </ul>
-
-
+        <List />
         <ButtonAll handleDeleteAll={handleDeleteAll}></ButtonAll>
       </Container>
-
     </>
   );
 }
